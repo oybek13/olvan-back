d95856d2074a396426a40748f47ea63c49afdc2e
oybek13 <oybek.karimjanov13@gmail.com>
2025-06-29 13:26:20 +0500

    auth url changed to api/api

--- src/main/java/brb/team/olvanback/controller/UserController.java
+++ src/main/java/brb/team/olvanback/controller/UserController.java
@@ -9,7 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/auth")
+@RequestMapping("/api/api/auth")
 @Tag(name = "Authentication")
 @CrossOrigin
 public class UserController {
