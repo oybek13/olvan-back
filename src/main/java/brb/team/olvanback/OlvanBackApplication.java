7afecbbbad00a20172c9a634cf8a3c76b0d90262
oybek13 <oybek.karimjanov13@gmail.com>
2025-07-02 13:32:34 +0500

    redeploy!

--- src/main/java/brb/team/olvanback/OlvanBackApplication.java
+++ src/main/java/brb/team/olvanback/OlvanBackApplication.java
@@ -9,7 +9,6 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 public class OlvanBackApplication {
 
     public static void main(String[] args) {
-
         SpringApplication.run(OlvanBackApplication.class, args);
     }
 
