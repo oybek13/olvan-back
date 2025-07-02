b9dea55bc286f51707711d7126fb7761b209079a
oybek13 <oybek.karimjanov13@gmail.com>
2025-07-02 14:10:06 +0500

    idnknow

--- src/main/java/brb/team/olvanback/exception/ExHandlerAdvice.java
+++ src/main/java/brb/team/olvanback/exception/ExHandlerAdvice.java
@@ -1,27 +1,18 @@
 package brb.team.olvanback.exception;
 
 import brb.team.olvanback.dto.CommonResponse;
-import brb.team.olvanback.exception.DataNotFoundException;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.dao.DataAccessException;
-import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import java.sql.SQLException;
-import java.time.LocalDateTime;
-import java.util.LinkedHashMap;
-import java.util.Map;
-
 @RestControllerAdvice
 @Slf4j
-public class ExceptionHandlerAdvice {
+public class ExHandlerAdvice {
 
     @ExceptionHandler(DataNotFoundException.class)
     public ResponseEntity<?> dataNotFoundException(DataNotFoundException exception) {
